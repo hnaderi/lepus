5b2c21546ed51ead33511f7f9a2e442200015ba8
Hossein Naderi <hossein-naderi@hotmail.com>
2022-05-14 19:22:27 +0430

    Added error types to ReplyCode

--- modules/protocol/src/main/scala/Constants.scala
+++ modules/protocol/src/main/scala/Constants.scala
@@ -16,30 +16,36 @@
 
 package lepus.protocol.constants
 
-val FrameMethod: Short = 1 //
-val FrameHeader: Short = 2 //
-val FrameBody: Short = 3 //
-val FrameHeartbeat: Short = 8 //
-val FrameMinSize: Short = 4096 //
-val FrameEnd: Short = 206 //
+val FrameMethod: Short = 1
+val FrameHeader: Short = 2
+val FrameBody: Short = 3
+val FrameHeartbeat: Short = 8
+val FrameMinSize: Short = 4096
+val FrameEnd: Short = 206
 
 enum ReplyCode(val code: Short) {
-  case ReplySuccess extends ReplyCode(200) //
-  case ContentTooLarge extends ReplyCode(311) // soft-error
-  case NoConsumers extends ReplyCode(313) // soft-error
-  case ConnectionForced extends ReplyCode(320) // hard-error
-  case InvalidPath extends ReplyCode(402) // hard-error
-  case AccessRefused extends ReplyCode(403) // soft-error
-  case NotFound extends ReplyCode(404) // soft-error
-  case ResourceLocked extends ReplyCode(405) // soft-error
-  case PreconditionFailed extends ReplyCode(406) // soft-error
-  case FrameError extends ReplyCode(501) // hard-error
-  case SyntaxError extends ReplyCode(502) // hard-error
-  case CommandInvalid extends ReplyCode(503) // hard-error
-  case ChannelError extends ReplyCode(504) // hard-error
-  case UnexpectedFrame extends ReplyCode(505) // hard-error
-  case ResourceError extends ReplyCode(506) // hard-error
-  case NotAllowed extends ReplyCode(530) // hard-error
-  case NotImplemented extends ReplyCode(540) // hard-error
-  case InternalError extends ReplyCode(541) // hard-error
+  case ReplySuccess extends ReplyCode(200)
+  case ContentTooLarge extends ReplyCode(311), ErrorCode(ErrorType.Channel)
+  case NoConsumers extends ReplyCode(313), ErrorCode(ErrorType.Channel)
+  case ConnectionForced extends ReplyCode(320), ErrorCode(ErrorType.Connection)
+  case InvalidPath extends ReplyCode(402), ErrorCode(ErrorType.Connection)
+  case AccessRefused extends ReplyCode(403), ErrorCode(ErrorType.Channel)
+  case NotFound extends ReplyCode(404), ErrorCode(ErrorType.Channel)
+  case ResourceLocked extends ReplyCode(405), ErrorCode(ErrorType.Channel)
+  case PreconditionFailed extends ReplyCode(406), ErrorCode(ErrorType.Channel)
+  case FrameError extends ReplyCode(501), ErrorCode(ErrorType.Connection)
+  case SyntaxError extends ReplyCode(502), ErrorCode(ErrorType.Connection)
+  case CommandInvalid extends ReplyCode(503), ErrorCode(ErrorType.Connection)
+  case ChannelError extends ReplyCode(504), ErrorCode(ErrorType.Connection)
+  case UnexpectedFrame extends ReplyCode(505), ErrorCode(ErrorType.Connection)
+  case ResourceError extends ReplyCode(506), ErrorCode(ErrorType.Connection)
+  case NotAllowed extends ReplyCode(530), ErrorCode(ErrorType.Connection)
+  case NotImplemented extends ReplyCode(540), ErrorCode(ErrorType.Connection)
+  case InternalError extends ReplyCode(541), ErrorCode(ErrorType.Connection)
 }
+
+enum ErrorType {
+  case Channel, Connection
+}
+
+sealed trait ErrorCode(val errorType: ErrorType)
