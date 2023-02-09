95ce871fef3baef3c5bd2c3f252a60554b50e0e3
Hossein Naderi <hossein-naderi@hotmail.com>
2023-02-09 18:28:06 +0000

    Update fs2-core, fs2-io, fs2-scodec to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val cats = "2.9.0"
     val catsEffect = "3.4.6"
-    val fs2 = "3.6.0"
+    val fs2 = "3.6.1"
     val scodec = "2.2.1"
     val scodecStream = "3.0.1"
     val scodecBit = "1.1.13"
