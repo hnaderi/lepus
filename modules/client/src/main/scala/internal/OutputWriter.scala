7e8c07f757f53624593bb0f60fe5d352403bbbf8
Hossein Naderi <hossein-naderi@hotmail.com>
2023-02-09 18:16:39 +0330

    Added OutputWriter

---
+++ modules/client/src/main/scala/internal/OutputWriter.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 Hossein Naderi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lepus.client.internal
+
+import cats.effect.Concurrent
+import cats.effect.kernel.Deferred
+import cats.implicits.*
+
+private[client] trait OutputWriter[F[_], T] {
+  def write(t: T): F[Unit]
+  def onClose: F[Unit]
+}
+
+private[client] object OutputWriter {
+  def apply[F[_]: Concurrent, T](out: T => F[Unit]): F[OutputWriter[F, T]] =
+    Deferred[F, ConnectionIsClosed.type]
+      .map(closed =>
+        new {
+          override def write(t: T): F[Unit] =
+            closed.tryGet.flatMap {
+              case Some(_) => ConnectionIsClosed.raiseError
+              case None =>
+                Concurrent[F].race(closed.get, out(t)).flatMap(_.liftTo)
+            }
+
+          override def onClose: F[Unit] =
+            closed.complete(ConnectionIsClosed).void
+        }
+      )
+  case object ConnectionIsClosed
+      extends RuntimeException("Connection is already closed!")
+}
---
+++ modules/client/src/test/scala/internal/OutputWriterSuite.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Hossein Naderi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lepus.client
+package internal
+
+import cats.effect.IO
+import cats.syntax.writer
+import lepus.client.internal.OutputWriter
+
+import scala.concurrent.duration.*
+
+class OutputWriterSuite extends InternalTestSuite {
+  test("Writes to underlying queue") {
+    for {
+      out <- InteractionList[Int]
+      writer <- OutputWriter(out.add(_))
+      _ <- writer.write(1)
+      _ <- out.assert(1)
+    } yield ()
+  }
+
+  test("Write fails after closing") {
+    for {
+      out <- InteractionList[Int]
+      writer <- OutputWriter(out.add(_))
+      _ <- writer.onClose
+      _ <- writer.write(1).intercept[OutputWriter.ConnectionIsClosed.type]
+      _ <- out.assert()
+    } yield ()
+  }
+
+  check("Interrupts blocked writes when closes") {
+    for {
+      writer <- OutputWriter(_ => IO.never)
+      _ <- IO
+        .both(
+          writer.onClose.delayBy(10.minutes),
+          writer.write(1)
+        )
+        .intercept[OutputWriter.ConnectionIsClosed.type]
+    } yield ()
+  }
+}
