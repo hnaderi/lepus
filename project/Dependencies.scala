a04e1b063d789d0b47f7e42a2d0a3bf975349569
Hossein Naderi <hossein-naderi@hotmail.com>
2026-08-24 04:32:03 +0330

    Update cats-effect-testkit to 3.7.1 (#466)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   object Versions {
     val cats = "2.9.0"
-    val catsEffect = "3.7.0"
+    val catsEffect = "3.7.1"
     val fs2 = "3.13.0"
     val scodec = "2.3.3"
     val scodecStream = "3.0.1"
