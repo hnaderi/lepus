32532d1c6c40a4884c8829f5b45c4c61c17c52aa
Hossein Naderi <hossein-naderi@hotmail.com>
2026-09-06 02:23:21 +0000

    Update fs2-io, fs2-scodec to 3.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val cats = "2.9.0"
     val catsEffect = "3.7.1"
-    val fs2 = "3.13.0"
+    val fs2 = "3.14.0"
     val scodec = "2.3.3"
     val scodecStream = "3.0.1"
     val scodecBit = "1.2.5"
